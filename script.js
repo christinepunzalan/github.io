fa0eff28cec6f2983bf33ba2f4f9ceec6db399e1
tinepunzalan <punzalanchristine88@gmail.com>
2024-11-24 06:20:28 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('modal-btn').addEventListener('change', function(event) {
-    if (event.target.checked) {
-        document.body.classList.add('no-scroll');
-    } else {
-        document.body.classList.remove('no-scroll');
-    }
-});
